75be8fd9813831bc12598d33ccdef1b78ea9dc44
Aditya Satish Karwa <aditya@adityas-mac-2.local>
2025-04-22 01:36:20 +0530

    Fix CSS loading issues for deployed site

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  build: {
+    cssMinify: true,
+    minify: true,
+    assetsInlineLimit: 0, // Prevents inlining of assets
+  },
 })
